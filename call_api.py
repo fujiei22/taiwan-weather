69edefc158c3acd602918f6216c941a3c851fa60
Tommy Sian <fujiei2238@gmail.com>
2025-09-10 12:10:40 +0800

    重寫  json 資料取值方式
    因為預計將時間當 row ，溫度、體感溫度當 column。所以將 時間、溫度、體感溫度一次 append 進 list 中

--- call_api.py
+++ call_api.py
@@ -1,8 +1,8 @@
+from dotenv import load_dotenv
+import os
 import requests
 import pandas as pd
 import json
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.environ["CWA_API_KEY"]
@@ -12,7 +12,6 @@ url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-089"
 # api parameter
 params = {
     "Authorization": API_KEY,
-    # "limit": 1,
     "format": "JSON",
     "LocationName": "新北市",
     "ElementName": "溫度,體感溫度",
@@ -22,50 +21,71 @@ params = {
 # call api
 reslut = requests.get(url, params=params).json()
 
-try:
-    with open("result.json", "w", encoding="utf-8") as f:
-        json.dump(reslut, f, ensure_ascii=False, indent=4)
-except:
-    pass
+# write data into a .json file
+with open("result.json", "w", encoding="utf-8") as f:
+    json.dump(reslut, f, indent=4, ensure_ascii=False)
+
+locations = reslut["records"]["Locations"][0]["Location"]
+weatherElement = locations[0]["WeatherElement"]
+tempTimeSeries = weatherElement[0]["Time"]
+appTempTimeSeries = weatherElement[1]["Time"]
+
+# all city name
+locationList = []
+for loc in locations:
+    locationList.append(loc["LocationName"])
+
+# all weahter element
+weatherElementList = []
+for ele in weatherElement:
+    weatherElementList.append(ele["ElementName"])
+
+# all time series
+row = []
+for t in range(len(tempTimeSeries)):
+    row.append({
+        "時間": tempTimeSeries[t]["DataTime"],
+        "溫度": tempTimeSeries[t]["ElementValue"][0]["Temperature"],
+        "體感溫度": appTempTimeSeries[t]["ElementValue"][0]["ApparentTemperature"]
+    })
+
+dfData = pd.DataFrame(row)
+print(dfData)
 
 
-# city name
-LocationName = str(reslut["records"]["Locations"][0]["Location"][0]["LocationName"])
-print(LocationName)
+# location = reslut["records"]["Locations"][0]["Location"][0] # 新北市
+# elements = location["WeatherElement"]
 
-location = reslut["records"]["Locations"][0]["Location"][0] # 新北市
-elements = location["WeatherElement"]
-
-# create element list for column name
-elementList = []
-for i in range(len(elements)):
-    elementName = elements[i]["ElementName"]
-    elementList.append(elementName)
-print(elementList)
+# # create element list for column name
+# elementList = []
+# for i in range(len(elements)):
+#     elementName = elements[i]["ElementName"]
+#     elementList.append(elementName)
+# print(elementList)
 
 
-# time quntity
-timeNumber = len(elements[0]["Time"])
+# # time quntity
+# timeNumber = len(elements[0]["Time"])
 
-# create time list for row name
-timeList = []
-for i in range(timeNumber):
-    eachTime = elements[0]["Time"][i]["DataTime"]
-    timeList.append(eachTime)
-print(timeList)
+# # create time list for row name
+# timeList = []
+# for i in range(timeNumber):
+#     eachTime = elements[0]["Time"][i]["DataTime"]
+#     timeList.append(eachTime)
+# print(timeList)
 
 
-# create Temperature, ApparentTemperature list
-tempList = []
-appTempList = []
+# # create Temperature, ApparentTemperature list
+# tempList = []
+# appTempList = []
 
-# append value to tempList, appTempList
-for i in range(timeNumber):
-    tempValue = elements[0]["Time"][i]["ElementValue"][0]["Temperature"]
-    tempList.append(tempValue)
+# # append value to tempList, appTempList
+# for i in range(timeNumber):
+#     tempValue = elements[0]["Time"][i]["ElementValue"][0]["Temperature"]
+#     tempList.append(tempValue)
 
-    appTempValue = elements[1]["Time"][i]["ElementValue"][0]["ApparentTemperature"]
-    appTempList.append(appTempValue)
+#     appTempValue = elements[1]["Time"][i]["ElementValue"][0]["ApparentTemperature"]
+#     appTempList.append(appTempValue)
 
-print(tempList)
-print(appTempList)
+# print(tempList)
+# print(appTempList)
